d7daa7ca1c9545d6d2823a7bc0cc36cb24903512
vancityjun <vancityjun@gmail.com>
2021-08-14 20:33:19 -0700

    initialize styled components with shared components

---
+++ src/shared/Content.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Content = (props: any) => <ContentStyle {...props} />
+
+const ContentStyle = styled.View`
+  margin-bottom: 10
+  line-height: 22
+`
+
+export default Content
